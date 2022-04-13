574384865fdff216c90b7533d738b4bad4968804
Tomáš Kelbasa <91247802+TomasKelbasa@users.noreply.github.com>
2022-04-13 10:01:03 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
-def on_forever():
-    pass
-basic.forever(on_forever)
+stred = [2,2]
+ctverec = [[1,0], [2,0], [1,1], [2,1]]
+odraz = [[1,0], [2,0], [1,1], [2,1]]
+
+zrcadli()
+
+def vykresli():
+    basic.clear_screen()
+    led.plot_brightness(stred[0], stred[1], 150)
+    for i in ctverec:
+        led.plot(i[0], i[1])
+    for e in odraz:
+       led.plot(e[0], e[1])
+
+def zrcadli():
+    for i in range(ctverec.length):
+        for e in range(0,2):
+            odraz[i][e] = ctverec[i][e] + 2*(stred[e]-ctverec[i][e])
+    vykresli()
+
+def on_button_pressed_a():
+    ctverec[1][0] += 1
+    ctverec[2][1] += 1
+    ctverec[3][0] += 1
+    ctverec[3][1] += 1
+    if(ctverec[3][0] == 5):
+        ctverec[1][0] -= 3
+        ctverec[2][1] -= 3
+        ctverec[3][0] -= 3
+        ctverec[3][1] -= 3
+    zrcadli()
+input.on_button_pressed(Button.A, on_button_pressed_a)
--- main.ts
+++ main.ts
@@ -1,3 +1,38 @@
-basic.forever(function on_forever() {
+let stred = [2, 2]
+let ctverec = [[1, 0], [2, 0], [1, 1], [2, 1]]
+let odraz = [[1, 0], [2, 0], [1, 1], [2, 1]]
+zrcadli()
+function vykresli() {
+    basic.clearScreen()
+    led.plotBrightness(stred[0], stred[1], 150)
+    for (let i of ctverec) {
+        led.plot(i[0], i[1])
+    }
+    for (let e of odraz) {
+        led.plot(e[0], e[1])
+    }
+}
+
+function zrcadli() {
+    for (let i = 0; i < ctverec.length; i++) {
+        for (let e = 0; e < 2; e++) {
+            odraz[i][e] = ctverec[i][e] + 2 * (stred[e] - ctverec[i][e])
+        }
+    }
+    vykresli()
+}
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    ctverec[1][0] += 1
+    ctverec[2][1] += 1
+    ctverec[3][0] += 1
+    ctverec[3][1] += 1
+    if (ctverec[3][0] == 5) {
+        ctverec[1][0] -= 3
+        ctverec[2][1] -= 3
+        ctverec[3][0] -= 3
+        ctverec[3][1] -= 3
+    }
     
+    zrcadli()
 })
